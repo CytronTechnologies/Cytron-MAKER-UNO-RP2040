8e2dcddc02934e739cc6728da3319eb447d17aac
noqman <140384051+noqman@users.noreply.github.com>
2024-02-27 17:06:50 +0800

    Update trig and echo pins

--- Example_Code/CircuitPython/Ultrasonic_Sensor_HC-SR04P/code.py
+++ Example_Code/CircuitPython/Ultrasonic_Sensor_HC-SR04P/code.py
@@ -4,10 +4,10 @@ This example code will uses Maker Uno RP2040 with Ultrasonic Sensor HC-SR04P to 
 to read distance from Ultrasonic Sensor HC-SR04P.
 
 CONNECTION:
-Grove 6 of Maker UNO RP2040 : HC-SR04P
+Grove 2 of Maker Uno RP2040 : HC-SR04P
 
-GP20 - Echo
-GP21 - Trig
+GP4 - Echo
+GP5 - Trig
 
 AUTHOR   : Cytron Technologies Sdn Bhd
 WEBSITE  : www.cytron.io
@@ -21,7 +21,7 @@ import digitalio
 import adafruit_hcsr04
 
 #Define pin GP20 and GP21 used on the board
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP20, echo_pin=board.GP21)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP4, echo_pin=board.GP5)
 
 while True:
     
